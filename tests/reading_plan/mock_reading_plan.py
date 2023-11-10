af490e4c4741f3b4bd44fc2da69bf7f329725ba5
jeferson-viana <jefersonv28@gmail.com>
2023-11-10 18:59:06 -0300

    Requisito 6 concluído

---
+++ tests/reading_plan/mock_reading_plan.py
@@ -0,0 +1,115 @@
+RESULT_MOCK = {'readable': [
+  {'unfilled_time': 1,
+   'chosen_news': [
+    ('10 jogos para iniciantes aprenderem a programar!', 12),
+    ('Estruturas de repetição: quais as 4 principais e quando usá-las?', 5)]},
+  {'unfilled_time': 1,
+   'chosen_news': [
+    ('Endless OS: por que vale a pena usar esse sistema', 12),
+    ('O que é array, para que serve e como fazer?', 5)]},
+  {'unfilled_time': 1,
+   'chosen_news': [
+    ('TrybeTalks – CTO do Nubank revela as habilidades de sucesso', 6),
+    ('Site responsivo: o que é e 10 dicas para aplicar', 11)]},
+  {'unfilled_time': 4,
+   'chosen_news': [
+    ('SaaS, GaaS, IaaS, DaaS, PaaS: entenda o modelo as a service', 14)]},
+  {'unfilled_time': 1,
+   'chosen_news': [
+    ('Armazenamento em nuvem: o que é e quais os 7 melhores?', 12),
+    ('5 exemplos de algoritmos na vida real e na computação', 5)]},
+  {'unfilled_time': 1,
+   'chosen_news': [
+    ('Componentização: o que é, por que usar e exemplo na prática!', 10),
+    ('Metadados: o que são, 20 exemplos e como gerenciar?', 7)]},
+  {'unfilled_time': 3,
+   'chosen_news': [
+    ('10 navegadores leves, rápidos e seguros para PC fraco!', 9),
+    ('Angular CLI: como usar essa interface de linha de comando?', 6)]},
+  {'unfilled_time': 1,
+   'chosen_news': [
+    ('Cabos de rede: o que são, quais os tipos e como crimpar?', 9),
+    ('Hardware e software: o que são e quais as diferenças?', 8)]},
+  {'unfilled_time': 1,
+   'chosen_news': [
+    ('Website development: o que é, o que faz e salário! O guia inicial!', 13),
+    ('Operadores booleanos: 3 exemplos para usar em pesquisas', 4)]},
+  {'unfilled_time': 7,
+   'chosen_news': [
+    ('Code Review: como virar um caçador de bugs com 15 dicas', 11)]},
+  {'unfilled_time': 6,
+   'chosen_news': [
+    ('Versionamento de software e de código: o que é e como fazer?', 12)]},
+  {'unfilled_time': 7,
+   'chosen_news': [
+    ('Usabilidade no Desenvolvimento de software e UX: como funciona?', 11)]},
+  {'unfilled_time': 8,
+   'chosen_news': [
+    ('Antivírus Android: conheça os 10 melhores e saiba por que usar!', 10)]},
+  {'unfilled_time': 9,
+   'chosen_news': [
+    ('Conheça os 11 tipos de firewall, saiba como funcionam e qual usar', 9)]},
+  {'unfilled_time': 9,
+   'chosen_news': [
+    ('Protocolo TCP/IP: o que é e exemplos de como funciona', 9)]},
+  {'unfilled_time': 4,
+   'chosen_news': [
+    ('Programação em Arduino para iniciantes em 11 passos!', 14)]},
+  {'unfilled_time': 0,
+   'chosen_news': [
+    ('Sistema Operacional Windows: versões, dicas e como instalar?', 18)]},
+  {'unfilled_time': 9,
+   'chosen_news': [
+    ('TrybeTalks — Gaules: os ensinamentos de um streamer do Brasil', 9)]},
+  {'unfilled_time': 5,
+   'chosen_news': [
+    ('Next JS: o que é, para que serve e por que usar?', 13)]},
+  {'unfilled_time': 8,
+   'chosen_news': [
+    ('Firewall: o que é, como funciona, 11 tipos e como configurar', 10)]},
+  {'unfilled_time': 9,
+   'chosen_news': [
+    ('Mark Zuckerberg é o criador do Metaverso?', 9)]},
+  {'unfilled_time': 9,
+   'chosen_news': [
+    ('Software livre: o que é, características, curiosidades e diferenças!', 9)
+    ]}],
+  'unreadable': []
+}
+
+RESULT_MOCK_2 = {
+  'readable': [
+    {'unfilled_time': 0,
+     'chosen_news': [
+       ('Operadores booleanos: 3 exemplos para usar em pesquisas', 4)]}],
+  'unreadable': [
+    ('10 jogos para iniciantes aprenderem a programar!', 12),
+    ('Endless OS: por que vale a pena usar esse sistema', 12),
+    ('TrybeTalks – CTO do Nubank revela as habilidades de sucesso', 6),
+    ('Site responsivo: o que é e 10 dicas para aplicar', 11),
+    ('SaaS, GaaS, IaaS, DaaS, PaaS: entenda o modelo as a service', 14),
+    ('Armazenamento em nuvem: o que é e quais os 7 melhores?', 12),
+    ('Componentização: o que é, por que usar e exemplo na prática!', 10),
+    ('10 navegadores leves, rápidos e seguros para PC fraco!', 9),
+    ('Cabos de rede: o que são, quais os tipos e como crimpar?', 9),
+    ('Estruturas de repetição: quais as 4 principais e quando usá-las?', 5),
+    ('Website development: o que é, o que faz e salário! O guia inicial!', 13),
+    ('Code Review: como virar um caçador de bugs com 15 dicas', 11),
+    ('Versionamento de software e de código: o que é e como fazer?', 12),
+    ('Metadados: o que são, 20 exemplos e como gerenciar?', 7),
+    ('Angular CLI: como usar essa interface de linha de comando?', 6),
+    ('O que é array, para que serve e como fazer?', 5),
+    ('Usabilidade no Desenvolvimento de software e UX: como funciona?', 11),
+    ('Antivírus Android: conheça os 10 melhores e saiba por que usar!', 10),
+    ('Conheça os 11 tipos de firewall, saiba como funcionam e qual usar', 9),
+    ('Protocolo TCP/IP: o que é e exemplos de como funciona', 9),
+    ('Programação em Arduino para iniciantes em 11 passos!', 14),
+    ('Sistema Operacional Windows: versões, dicas e como instalar?', 18),
+    ('5 exemplos de algoritmos na vida real e na computação', 5),
+    ('TrybeTalks — Gaules: os ensinamentos de um streamer do Brasil', 9),
+    ('Next JS: o que é, para que serve e por que usar?', 13),
+    ('Hardware e software: o que são e quais as diferenças?', 8),
+    ('Firewall: o que é, como funciona, 11 tipos e como configurar', 10),
+    ('Mark Zuckerberg é o criador do Metaverso?', 9),
+    ('Software livre: o que é, características, curiosidades e diferenças!', 9)
+    ]}
--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,30 @@
 from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tests.reading_plan.mock_reading_plan import RESULT_MOCK, RESULT_MOCK_2
+import pytest
+from unittest.mock import Mock, patch
+from tests.reading_plan.mock_find_news import MOCK_FIND_NEWS
 
 
 def test_reading_plan_group_news():
-    pass
+    FILE_MOCK = ('tech_news.analyzer.reading_plan.ReadingPlanService'
+                 '._db_news_proxy')
+    with pytest.raises(ValueError,
+                       match="Valor 'available_time' deve ser maior que zero"):
+        ReadingPlanService.group_news_for_available_time(0)
+
+    mock_find_news = Mock(return_value=MOCK_FIND_NEWS)
+    with patch(FILE_MOCK, mock_find_news):
+        result = ReadingPlanService.group_news_for_available_time(18)
+
+        assert result['unreadable'] == RESULT_MOCK['unreadable']
+        assert RESULT_MOCK['readable'][0] in result['readable']
+        assert len(result['readable']) == 22
+        assert len(result['unreadable']) == 0
+
+    mock_find_news = Mock(return_value=MOCK_FIND_NEWS)
+    with patch(FILE_MOCK, mock_find_news):
+        result = ReadingPlanService.group_news_for_available_time(4)
+        assert len(result['readable']) == 1
+        assert len(result['unreadable']) == 29
+        assert result['unreadable'] == RESULT_MOCK_2['unreadable']
+        assert RESULT_MOCK_2['readable'][0] in result['readable']
