f7234ba21743fb1b36c57d157f105822f9810308
jeferson-viana <jefersonv28@gmail.com>
2023-11-10 11:31:45 -0300

    Requisito 4 concluído

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -34,15 +34,69 @@ def scrape_next_page_link(html_content):
     return next_page
 
 
-test = scrape_next_page_link(fetch('https://blog.betrybe.com/'))
-print(test)
-# print(len(test))
+def formated_url(selector: Selector) -> str:
+    href = selector.css('link[rel="canonical"]::attr(href)').get()
+    if href:
+        return href
+    return ' '
+
+
+def formated_title(selector: Selector) -> str:
+    title = selector.css('h1.entry-title::text').get()
+    if title:
+        return title.replace('\xa0', '')
+    return ' '
+
+
+def formated_timestamp(selector: Selector) -> str:
+    timestamp = selector.css('li.meta-date::text').get()
+    if timestamp:
+        return timestamp
+    return ' '
+
+
+def formated_author(selector: Selector) -> str:
+    author = selector.css('span.fn a::text').get()
+    if author:
+        return author.replace('\t', "").replace('\n', "")
+    return ' '
+
+
+def formated_reading_time(selector: Selector) -> int:
+    reading_time = selector.css('li.meta-reading-time::text').get()
+    if reading_time:
+        return int(reading_time.split(' ')[0])
+    return 0
+
+
+def formated_summary(selector: Selector) -> str:
+    div = 'div.entry-content'
+    summary = selector.css(div).css('p')[0].css('p *::text').getall()
+    summary = ''.join(summary).replace('\xa0', '')
+    if summary[-1] == ' ':
+        return summary[:-1]
+    return summary
+
+
+def formated_category(selector: Selector) -> str:
+    category = selector.css('span.label::text').get()
+    if category:
+        return category
+    return ' '
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    tech_new = {}
+    selector = Selector(text=html_content)
+    tech_new['url'] = formated_url(selector)
+    tech_new['title'] = formated_title(selector)
+    tech_new['timestamp'] = formated_timestamp(selector)
+    tech_new['writer'] = formated_author(selector)
+    tech_new['reading_time'] = formated_reading_time(selector)
+    tech_new['summary'] = formated_summary(selector)
+    tech_new['category'] = formated_category(selector)
+    return tech_new
 
 
 # Requisito 5
