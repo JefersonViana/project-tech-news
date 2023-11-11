02b090500bec5ac6309cd3497774eb5e351e8cfe
jeferson-viana <jefersonv28@gmail.com>
2023-11-10 21:23:55 -0300

    Requisito 8 concluído

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,28 @@
-from tech_news.database import search_news
+from tech_news.database import search_news  # , db
+from datetime import datetime
+
+# array = [
+#     {
+#         "url": "https://blog.betrybe.com/novidades/noticia-bacana",
+#         "title": "Notícia bacana",
+#         "writer": "Eu",
+#         "summary": "Algo muito bacana aconteceu",
+#         "reading_time": 4,
+#         "timestamp": "04/04/2021",
+#         "category": "Ferramentas",
+#     },
+#     {
+#         "url": "https://blog.betrybe.com/novidades/noticia-legal",
+#         "title": "Notícia bacana 2",
+#         "writer": "Você",
+#         "summary": "Algo muito bacana aconteceu de novo",
+#         "reading_time": 1,
+#         "timestamp": "07/04/2022",
+#         "category": "Novidades",
+#     }
+# ]
+# db.news.delete_many({})
+# db.news.insert_many(array)
 
 
 # Requisito 7
@@ -12,8 +36,22 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        data_datetime = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    data_formatada = data_datetime.strftime('%d/%m/%Y')
+    result = [
+        (new['title'], new['url'])
+        for new in search_news({'timestamp': data_formatada})
+    ]
+    return result
+
+
+# test = search_by_date('21-12-1980')
+# print(test)
+# print(len(test))
 
 
 # Requisito 9
